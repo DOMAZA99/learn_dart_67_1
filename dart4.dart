9f69db94f88d2428965241da6419953aba810df9
panu <panusomjai@gmail.com>
2025-07-26 15:08:32 +0700

    Step 4

---
+++ dart4.dart
@@ -0,0 +1,10 @@
+void main() {
+  dynamic dynamicVariable = 42;
+  print(dynamicVariable); // Output: 42
+  
+  dynamicVariable = "Hello, Dart!";
+  print(dynamicVariable); // Output: Hello, Dart! 
+
+  dynamicVariable = [1, 2, 3];
+  print(dynamicVariable); // Output: [1, 2, 3]
+}
