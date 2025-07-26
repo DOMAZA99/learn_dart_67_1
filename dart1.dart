ee2a3e1f3a2bffb70e8aea80281d66a2207d0dc9
panu <panusomjai@gmail.com>
2025-07-26 14:02:44 +0700

    Step 1: Structure

---
+++ dart1.dart
@@ -0,0 +1,24 @@
+// Structure in dart
+void main() {
+  var name = "John";
+  print(name); 
+
+  print("your name is $name.");
+  print("your name is ${name = 'Jane'}.");
+
+  int age = 34;
+print("your age is $age.");
+
+//Good varible names
+String userName = "Alice";
+int userAge = 12;
+double temperature = 36.6;
+bool isActive = true;
+
+// Bad variable names
+// bool isInactive = ture; // bool isActive = false;
+// sting  1name = "Bob";
+// int max count = 50;
+// double totalPercent = 100.0; // totalPercent
+// var if = "Error"; // reserved keyword
+}
