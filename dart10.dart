7640e46537b19afb6f8224e420609f5bfd9d294e
panu <panusomjai@gmail.com>
2025-08-02 11:15:45 +0700

    Step10

---
+++ dart10.dart
@@ -0,0 +1,29 @@
+// Interface
+
+void main() {
+  Lion lion = new Lion();
+  lion.displayType();
+  lion.displayArea();
+}
+
+class Animal {
+  void displayType() {
+    print("Animal");
+  }
+
+  void displayArea() {
+    print("Earth");
+  }
+}
+
+
+// class Lion implementing Animal
+class Lion implements Animal {
+  void displayType() {
+    print("Lion");
+  }
+
+  void displayArea() {
+    print("Forest");
+  }
+}
